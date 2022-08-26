ff1cb66881b68d0190596ec50d3343262d0bd1ba
tao.yang <tao.yang@funi365.com>
2022-08-27 01:50:40 +0800

    update

--- src/pinata.js
+++ src/pinata.js
@@ -38,31 +38,10 @@ export const uploadFileToIPFS = async(file) => {
     let data = new FormData();
     data.append('file', file);
 
-    const metadata = JSON.stringify({
-        name: 'testname',
-        keyvalues: {
-            exampleKey: 'exampleValue'
-        }
-    });
-    data.append('pinataMetadata', metadata);
+    data.append('pinataMetadata', '{"name": "MyFile", "keyvalues": {"company": "Pinata"}}');
 
     //pinataOptions are optional
-    const pinataOptions = JSON.stringify({
-        cidVersion: 0,
-        customPinPolicy: {
-            regions: [
-                {
-                    id: 'FRA1',
-                    desiredReplicationCount: 1
-                },
-                {
-                    id: 'NYC1',
-                    desiredReplicationCount: 2
-                }
-            ]
-        }
-    });
-    data.append('pinataOptions', pinataOptions);
+    data.append('pinataOptions', '{"cidVersion": 1}');
 
     return axios 
         .post(url, data, {
